87c81e1cb8b1e1432daf48b5189185c9aeb199b9
swshadows <joelson.jpl@gmail.com>
2023-03-25 14:22:18 -0400

    fix: Consertando paths

--- src/views/templates/footer.php
+++ src/views/templates/footer.php
@@ -5,7 +5,7 @@
 		<a href="#">Documentação</a>
 	</div>
 </footer>
-<script src="js/main.js"></script>
+<script src="/js/main.js"></script>
 
 </body>
 
--- src/views/templates/header.php
+++ src/views/templates/header.php
@@ -11,8 +11,8 @@ $is_logged = App::check_auth();
 	<meta charset="UTF-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<link rel="stylesheet" href="css/main.css">
-	<link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
+	<link rel="stylesheet" href="/css/main.css">
+	<link rel="shortcut icon" href="/favicon.ico" type="image/x-icon">
 	<title><?= __NAME__ ?></title>
 </head>
 
@@ -20,7 +20,7 @@ $is_logged = App::check_auth();
 
 	<header class="header">
 		<div class="links wrapper">
-			<a href="/"><img width="50" src="favicon.ico" alt="Ícone da aplicação"></a>
+			<a href="/"><img width="50" src="/favicon.ico" alt="Ícone da aplicação"></a>
 			<a href="/"><span><?= __NAME__ ?></span></a>
 		</div>
 		<?php if ($is_logged) : ?>
